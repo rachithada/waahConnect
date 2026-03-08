b422097a7b05f3ca6b172cea30b68827bbc69b03
rachithada <rachitaug12@gmail.com>
2026-03-08 22:02:16 +0530

    Origin added

--- backend/main.py
+++ backend/main.py
@@ -10,9 +10,15 @@ ADMIN_KEY = "superadmin123"
 
 app = FastAPI()
 
+origins = [
+    "https://waah-connect.vercel.app",
+    "http://localhost:5173",
+    "http://localhost:3000"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
